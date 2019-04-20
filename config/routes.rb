04293b76c3e93d0de94d357634f8537a7cd187f3
Jacqi Entzminger <jackie.entz@gmail.com>
2019-04-20 15:20:32 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+ #alterative option ~> get 'posts/:id', to: 'posts#show'
+ resources :posts, only: :show #restful route option 
 end
